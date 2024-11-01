e8e236cb9efd41811da7c25d241bdf9d8f2988ae
Porche123 <115189897+Porche123@users.noreply.github.com>
2024-11-02 00:31:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const nav1 = document.getElementById('nav1')
+nav2 = document.getElementById('nav2')
+nav3 = document.getElementById('nav3')
+nav4 = document.getElementById('nav4')
+
+
+
+nav1.onclick = function() {
+console.log("yahya")
+  
+}
